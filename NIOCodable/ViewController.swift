0753f9c96d0cb842b6e7fd0c6560abd55e966ed8
chunyang li <chunyang.li.o@nio.com>
2019-07-24 11:06:11 +0800

    Initial Commit

---
+++ NIOCodable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NIOCodable
+//
+//  Created by Chunyang Li 李春阳 on 2019/7/24.
+//  Copyright © 2019 com.nio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NIOCodableTests/NIOCodableTests.swift
@@ -0,0 +1,34 @@
+//
+//  NIOCodableTests.swift
+//  NIOCodableTests
+//
+//  Created by Chunyang Li 李春阳 on 2019/7/24.
+//  Copyright © 2019 com.nio. All rights reserved.
+//
+
+import XCTest
+@testable import NIOCodable
+
+class NIOCodableTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
